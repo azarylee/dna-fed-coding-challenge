59e403101c3370b8223ab0e4cc6516e9a8d136c4
azarylee <liyijun430@hotmail.com>
2020-01-13 00:11:57 +1100

    build dist

--- build/webpack.pro.js
+++ build/webpack.pro.js
@@ -2,13 +2,14 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const base = require("./webpack.base.js");
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+// const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = merge(base, {
     mode: 'production',
+
     plugins: [
-        new CleanWebpackPlugin(['dist'], {
-            root: path.resolve(__dirname, '../'),
-        })
+        // new CleanWebpackPlugin(['dist'], {
+        //     root: path.resolve(__dirname, '../'),
+        // })
     ],
 })
